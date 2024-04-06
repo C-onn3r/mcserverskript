990f6716a73efe97c0e1c689e284845669c649a0
C-onner <166254418+C-onn3r@users.noreply.github.com>
2024-04-06 23:50:05 +0200

    Add files via upload

---
+++ minecraftinstall.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo "Minecraft Server installer Skript von C-onner -> https://c-onner.de"
+
+read -p "Möchtest du die Installation starten? (ja/nein): " choice
+case "$choice" in
+  j|J|ja|Ja ) echo "Weiter geht's...";;
+  * ) echo "Abbruch."; exit;;
+esac
+
+java -version > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Java ist bereits installiert."
+else
+  echo "Java ist nicht installiert!"
+  read -p "Welche Java-Version soll installiert werden? (11/17): " java_version
+  sudo apt update
+  sudo apt install openjdk-$java_version-jdk-headless -y
+fi
+
+read -p "Wie soll der Ordner heißen, in dem der Server installiert werden soll? " folder_name
+mkdir $folder_name
+cd $folder_name
+
+read -p "Welche Minecraft Server-Version möchtest du installieren? (1.19.4, 1.18.2, 1.17.1, 1.16.5): " minecraft_version
+case "$minecraft_version" in
+  1.19.4 ) wget https://media.c-onner.det/_/download/scripts/minecraft/1.19/paper.jar;;
+  1.18.2 ) wget https://media.c-onner.det/_/download/scripts/minecraft/1.18/paper.jar;;
+  1.17.1 ) wget https://media.c-onner.det/_/download/scripts/minecraft/1.17/paper.jar;;
+  1.16.5 ) wget https://media.c-onner.det/_/download/scripts/minecraft/1.16/paper.jar;;
+  * ) echo "Ungültige Eingabe. Abbruch."; exit;;
+esac
+
+echo "Die Server .jar wurde erfolgreich installiert."
+
+read -p "Wie viel GB RAM soll der Server verwenden? (2/4/6/8/16 GB): " ram_amount
+case "$ram_amount" in
+  2|4|6|8|16 ) ;;
+  * ) echo "Ungültige Eingabe. Abbruch."; exit;;
+esac
+
+echo "screen -S minecraftserver java -Xms${ram_amount}G -Xmx${ram_amount}G -jar paper.jar" > start.sh
+sudo apt install screen -y
+chmod +x start.sh
+
+echo "Dein Server wurde erfolgreich eingerichtet!"
+echo "Beachte, dass du die eula.txt noch akzeptieren musst!"
+echo "Erledige dies einfach mit 'nano eula.txt'."
+echo "Deinen Server kannst du übrigens mit './start.sh' starten!"
+./start.sh
